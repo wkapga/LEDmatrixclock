26733e31a092f18f956561e6c2dfb14c98967064
gabi <gabriel.panzenboeck@gmail.com>
2013-03-06 23:11:10 +0100

    cleanup2

--- _2013_03_06_LEDmatrixclock.ino
+++ _2013_03_06_LEDmatrixclock.ino
@@ -30,7 +30,6 @@ unsigned int localPort = 8888;      // local port to listen for UDP packets
 IPAddress timeServer(91,118,20,250); // 0.at.pool.ntp.org
 
 
-unsigned int localPort = 8888;      // local port to listen for UDP packets
 
 // IPAddress timeServer(132, 163, 4, 101); // time-a.timefreq.bldrdoc.gov NTP server
 // IPAddress timeServer(132, 163, 4, 102); // time-b.timefreq.bldrdoc.gov NTP server
@@ -85,56 +84,6 @@ void setup() {
 
 }
 
-
-void getNTPtime() {
-
-sendNTPpacket(timeServer); // send an NTP packet to a time server
-
-    // wait to see if a reply is available
-  delay(1000);  
-  if ( Udp.parsePacket() ) {  
-    // We've received a packet, read the data from it
-    Udp.read(packetBuffer,NTP_PACKET_SIZE);  // read the packet into the buffer
-
-    //the timestamp starts at byte 40 of the received packet and is four bytes,
-    // or two words, long. First, esxtract the two words:
-
-    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-    unsigned long secsSince1900 = highWord << 16 | lowWord;    
-    unsigned long epoch = secsSince1900 - 2208988800UL;
-    
-    myhour = (epoch  % 86400L) / 3600 ;
-    myminute = (epoch  % 3600) / 60;
-
-  }
-}
-
-
-
-unsigned long sendNTPpacket(IPAddress& address)
-{
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE); 
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49; 
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp: 		   
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
-  Udp.write(packetBuffer,NTP_PACKET_SIZE);
-  Udp.endPacket(); 
-
-}
 
 
 void getNTPtime() {
